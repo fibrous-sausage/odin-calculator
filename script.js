59b70c60b9c7f4d218ca428bb2d6bdf7ba716415
fibrous <65938238+fibrous-sausage@users.noreply.github.com>
2025-02-09 19:24:46 -0500

    Add logic to operate on the basic four functions

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const add = (num1, num2) => num1 + num2;
+const sub = (num1, num2) => num1 - num2;
+const mul = (num1, num2) => num1 * num2;
+const div = (num1, num2) => num1 / num2;
+
+const operate = (operator, num1, num2) => {
+    switch (operator) {
+        case "+": return add(num1, num2);
+        case "-": return sub(num1, num2);
+        case "×": return mul(num1, num2);
+        case "÷": return div(num1, num2);
+        default: return num1;
+    }
+}
